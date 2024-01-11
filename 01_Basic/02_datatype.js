f68630d0ee1984bd769e6ebcd644dad0455d3a85
Aakash chouhan <114312978+aakashchauhan1547@users.noreply.github.com>
2024-01-11 21:06:53 +0000

    datatype in javascript

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,28 @@
+//"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs , not using browser
+console.log(3+
+    3); // code readability
+
+    console.log("Aaakash");
+
+
+    let name = "vikas";
+    let age = 15
+    let isLoggedIn = false
+
+
+  //  number => 2 to power 53
+  //  bigint
+  //  string =>" jjj"
+  //  boolean => true/false
+  //  null => standalone value ---let city = null
+  //undefind ----- let city;
+  //symbol => unique
+
+  // object
+
+  console.log(typeof age );
+
+
+
