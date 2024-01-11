f4d6f1c1a3ecaf2a30fbf22175046153f01e84b0
Aakash chouhan <114312978+aakashchauhan1547@users.noreply.github.com>
2024-01-11 20:20:17 +0000

    variables in javascript

---
+++ 01_Basic/Variables1.js
@@ -0,0 +1,11 @@
+const accountId = 45823699714
+let emailId = "aakashch@gmail.com"
+var password = 123456 //this variable not used in javascript because of issues in block scope and functional scope
+accountCity = "Mumbai"
+
+//accountId = 12345679 It is can't re-declear and update
+emailId = "vikas@gmail.com"
+password = 987654321
+
+accountCity = "varanasi"
+console.table([accountId , password , emailId , accountCity])
